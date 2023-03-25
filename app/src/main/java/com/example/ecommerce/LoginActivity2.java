ae23587b7cfb79ca0c2b9a07c521aae188e70664
Hicham Rahmouni <hicrahtubi@gmail.com>
2023-03-25 21:41:54 +0200

    Admin link

--- app/src/main/java/com/example/ecommerce/LoginActivity2.java
+++ app/src/main/java/com/example/ecommerce/LoginActivity2.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ecommerce.R;
@@ -33,8 +34,11 @@ public class LoginActivity2 extends AppCompatActivity {
     private EditText InputPhoneNumber, InputPassword;
     private Button LoginButton;
     private ProgressDialog loadingBar;
-    private String parentDbName="Users";
+
+    private TextView AdminLink, NotAdminLink;
+    private String parentDbName = "Users";
     private CheckBox ChkBoxRememberMe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +47,8 @@ public class LoginActivity2 extends AppCompatActivity {
         LoginButton = (Button) findViewById(R.id.login_btn);
         InputPassword = (EditText) findViewById(R.id.login_password_input);
         InputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        AdminLink = (TextView) findViewById(R.id.admin_panel_link);
+        NotAdminLink = (TextView) findViewById(R.id.not_admin_panel_link);
         loadingBar = new ProgressDialog(this);
         ChkBoxRememberMe = (CheckBox) findViewById(R.id.remember_me_chkb);
         Paper.init(this);
@@ -54,75 +60,106 @@ public class LoginActivity2 extends AppCompatActivity {
                 loginUser();
             }
 
-            private void loginUser() {
-                String phone = InputPhoneNumber.getText().toString();
-                String password = InputPassword.getText().toString();
-
-                 if(TextUtils.isEmpty(phone)){
-
-                     Toast.makeText(getApplicationContext(), "plese write your phone number", Toast.LENGTH_SHORT).show();
-                }
-                else if(TextUtils.isEmpty(password)){
-
-                    Toast.makeText(getApplicationContext(), "plese write your password", Toast.LENGTH_SHORT).show();
-                }
-                 else{
-                     loadingBar.setTitle("login account");
-                     loadingBar.setMessage("Please wait while we are checking teh credentials");
-                     loadingBar.setCanceledOnTouchOutside(false);
-                     loadingBar.show();
-
-                     AllowAccessToAccount(phone, password);
-                 }
-
+            // addedd }} here
+        });
+        AdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginButton.setText("Login Admin");
+                AdminLink.setVisibility(View.INVISIBLE);
+                NotAdminLink.setVisibility(View.VISIBLE);
+                parentDbName = "Admins";
             }
+        });
+        NotAdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginButton.setText("Login");
+                AdminLink.setVisibility(View.VISIBLE);
+                NotAdminLink.setVisibility(View.INVISIBLE);
+                parentDbName = "Users";
 
-            private void AllowAccessToAccount(String phone, String password) {
-                if(ChkBoxRememberMe.isChecked()){
-
-                    Paper.book().write(Prevalent.UserPhoneKey,phone);
-                    Paper.book().write(Prevalent.UserPasswordKey,password);
-                }
-                final DatabaseReference RootRef ;
-                RootRef = FirebaseDatabase.getInstance().getReference().child("Users");
-
-                RootRef.orderByChild("phone").equalTo(phone).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                            if (snapshot.exists()) {
-                                Toast.makeText(getApplicationContext(), "kokokokoko", Toast.LENGTH_LONG).show();
-                                //Users usersData = snapshot.child(parentDbName).child(phone).getValue(Users.class);
-                                Map<String, Object> map = (Map<String, Object>)   snapshot.getValue();
-                                Log.d("AnyTagYouWant", map.toString());
-
-                                //Toast.makeText(getApplicationContext(), (String) map.get("phone"), Toast.LENGTH_LONG).show();
-                                Map<String, Object> nestedmap = (Map)map.get("phone");
-                                Users usersData = new Users((String) nestedmap.get("name"),(String)nestedmap.get("phone"),(String)nestedmap.get("password"));
-                                Log.d("TagYouWant", usersData.toString());
-
-                                if(usersData.getPhone().equals(phone)){
-                                    if(usersData.getPassword().equals(password)){
-                                        Toast.makeText(getApplicationContext(), "Logged in successfully", Toast.LENGTH_SHORT).show();
-                                        loadingBar.dismiss();
-                                        Intent intent = new Intent(LoginActivity2.this, HomeActivity.class);
-                                        startActivity(intent);
-
-                                    }
-                                }
-                            } else {
-                                Toast.makeText(getApplicationContext(), "the account with this phone number"+phone+"does not exist", Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
-                            }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
             }
         });
     }
+
+
+    private void loginUser() {
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(phone)) {
+
+            Toast.makeText(getApplicationContext(), "plese write your phone number", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+
+            Toast.makeText(getApplicationContext(), "plese write your password", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("login account");
+            loadingBar.setMessage("Please wait while we are checking teh credentials");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            AllowAccessToAccount(phone, password);
+        }
+
+    }
+
+    private void AllowAccessToAccount(String phone, String password) {
+        if (ChkBoxRememberMe.isChecked()) {
+
+            Paper.book().write(Prevalent.UserPhoneKey, phone);
+            Paper.book().write(Prevalent.UserPasswordKey, password);
+        }
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();//.child("Admins");
+        //orderByChild("admin").equalTo(password).
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                if (snapshot.exists()) {
+                    Toast.makeText(getApplicationContext(), "kokokokoko", Toast.LENGTH_LONG).show();
+                    //Users usersData = snapshot.child(parentDbName).child(phone).getValue(Users.class);
+                    Map<String, Object> map = (Map<String, Object>) snapshot.getValue();
+                    Log.d("AnyTagYouWant", map.toString());
+
+                    //Toast.makeText(getApplicationContext(), (String) map.get("phone"), Toast.LENGTH_LONG).show();
+                    Map<String, Object> nestedmap0 = (Map) map.get(parentDbName);
+                    Map<String, Object> nestedmap = (Map) nestedmap0.get(phone);
+                    Users usersData = new Users(String.valueOf(nestedmap.get("name")), String.valueOf(nestedmap.get("phone")), String.valueOf(nestedmap.get("password")));
+                    Log.d("TagYouWant", usersData.toString());
+
+                    if (usersData.getPhone().equals(phone)) {
+                        if (usersData.getPassword().equals(password)) {
+                            if (parentDbName.equals("Admins")) {
+                                Toast.makeText(getApplicationContext(), "Welcome Admin you Logged in successfully", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent intent = new Intent(LoginActivity2.this, AdminAddNewPorductActivity.class);
+                                startActivity(intent);
+                            } else if (parentDbName.equals("Users")) {
+                                Toast.makeText(getApplicationContext(), "Logged in successfully", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent intent = new Intent(LoginActivity2.this, HomeActivity.class);
+                                startActivity(intent);
+                            }
+
+                        }
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "the account with this phone number" + phone + " does not exist", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }//matched
 }
+
+
